fa3c16ab4124965039ff2f03b82babc9a81395be
jsmillie7 <31022078+jsmillie7@users.noreply.github.com>
2019-03-06 22:11:26 -0700

    Update csvtest.py

--- csvtest.py
+++ csvtest.py
@@ -1,40 +1,52 @@
 import csv
 
-'''
-Set Up
-'''
+def SOPsearch():
+    file = 'test.csv' # CSV data file
+    matches = [] # list of strings used to search for SOP name
+    pos = 1  # used to identify multiple similarly named tests by position
 
-file = 'test.csv'
+    # Create Dictionary from Data File
+    with open(file) as f:
+        d = dict(filter(None, csv.reader(f)))
 
-with open(file) as f:
-    d = dict(filter(None, csv.reader(f)))
+    # Add names of SOPs to a searchable list
+    with open(file) as f:
+        reader = csv.reader(f, delimiter=",")
+        for i in reader:
+            matches.append(i[0])
+
+    print("press 'x' at any time to exit.")
+
+    search = input('search term: ')
+
+    if search == 'x':
+        return
+
+    indices = [i for i, s in enumerate(matches) if search in s] # adds matching results to the list
 
 
-matches = []
-with open(file) as f:
-    reader = csv.reader(f, delimiter=",")
-    for i in reader:
-        matches.append(i[0])
+    if len(indices) == 1:
+        print(d[matches[indices[0]]],matches[indices[0]])
 
-# print(matches)
+    elif len(indices) > 1:
+        for i in indices:
+            print('%s)' % pos, d[matches[i]], matches[i])
+            pos += 1
 
-search = input('Search Term: ')
+        select = input('select test: ')
 
-indices = [i for i, s in enumerate(matches) if search in s]
-pos = 1
+        if select.isdigit() == True and int(select) <= len(indices):
+            print(d[matches[indices[int(select)-1]]])
 
-if len(indices) == 1:
-    print(d[matches[indices[0]]])
+        elif select == 'x':
+            return
 
-elif len(indices) > 1:
-    for i in indices:
-        print('%s)' % pos, d[matches[i]], matches[i])
-        pos += 1
-    select = input('Select Test: ')
-    if select.isdigit() == True and int(select) <= len(indices):
-        print(d[matches[indices[int(select)-1]]])
+        else:
+            print('invalid selection. try again.')
+            SOPsearch()
+
     else:
-        print('invalid selection')
+        print('no matches. try again.')
+        SOPsearch()
 
-else:
-    print('no matches')
+SOPsearch()
